1a9318e437585b316353a9ebe7ec1934ad1e167a
Alexey Yudin <alexey.y.yudin@gmail.com>
2020-03-08 18:19:22 +0100

    Add form solution.

--- src/components/currency-form/currency-form.js
+++ src/components/currency-form/currency-form.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './currency-form.css';
 
 export function CurrencyForm() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
+
+  function amountHandler(event) {
+    const value = event.target.value;
+    setAmount(value);
+  }
+
+  function fromCurrencyHandler(event) {
+    const value = event.target.value;
+    setFromCurrency(value);
+  }
+
+  function toCurrencyHandler(event) {
+    const value = event.target.value;
+    setToCurrency(value);
+  }
+
+  function onClickHandler() {
+    console.log(amount, fromCurrency, toCurrency);
+  }
+
   return (
     <div>
-      <input type="text" placeholder="Amount"/>
-      <select>
+      <input type="number" min={0} value={amount} placeholder="Amount" onChange={amountHandler}/>
+
+      <select value={fromCurrency} onChange={fromCurrencyHandler}>
         <option value={null}>Select currency</option>
+        <option value={'USD'}>USD</option>
+        <option value={'EUR'}>EUR</option>
       </select>
-      <select>
+
+      <select value={toCurrency} onChange={toCurrencyHandler}>
         <option value={null}>Select currency</option>
+        <option value={'USD'}>USD</option>
+        <option value={'EUR'}>EUR</option>
       </select>
-      <button type="button">Exchange</button>
+
+      <button type="button" onClick={onClickHandler}>Exchange</button>
     </div>
   );
 }
