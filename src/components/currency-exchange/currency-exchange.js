91d506e3d3ee495dd44609ea582646b528c593d1
Alexey Yudin <alexey.y.yudin@gmail.com>
2020-03-08 17:45:29 +0100

    Hooks example.

--- src/components/currency-exchange/currency-exchange.js
+++ src/components/currency-exchange/currency-exchange.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './currency-exchange.css';
 import {PageTitle} from "../page-title/page-title";
 import {CurrencyResult} from "../currency-result/currency-result";
 import {CurrencyForm} from "../currency-form/currency-form";
 
 export function CurrencyExchange() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(count);
+  });
+
+  function onClickHandler() {
+    setCount(previousCount => {
+      return previousCount + 1;
+    });
+  }
+
   return (
     <div>
+      <button type="button" onClick={onClickHandler}>Click me</button>
       <PageTitle/>
       <CurrencyForm/>
       <CurrencyResult/>
+
     </div>
   );
 }
